700c61bda27cd0525b7f182a4938419832eeb5e0
jeferson <jefferson78111@gmail.com>
2023-02-19 15:46:17 -0500

    Nombre del proyecto: my-app, Descripción: versión final del proyecto my-app

---
+++ src/main/java/com/jeferson/myapp/entity/TestEntity.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.jeferson.myapp.entity;
+
+/**
+ * @author Jeferson Steven
+ *
+ */
+public class TestEntity {
+	/**
+	 * Propiedad de prueba para la muestra de compilación del archivo testentity.
+	 */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
